55b88b7f6386fc89be3039319bccbe1dfd88bf9d
Muhammad Din Al Ayubi <115516424+mdinalayubi@users.noreply.github.com>
2024-05-07 21:48:57 +0700

    Delete sistem-perpustakaan-qr-code directory

--- sistem-perpustakaan-qr-code/app/Views/imports/scripts/admin.php
+++
@@ -1,3 +0,0 @@
-<script src="<?= base_url("assets/js/sidebarmenu.js") ?>"></script>
-<script src="<?= base_url("assets/js/app.min.js") ?>"></script>
-<script src="<?= base_url("assets/libs/simplebar/simplebar.js") ?>"></script>
--- sistem-perpustakaan-qr-code/app/Views/imports/scripts/basic_scripts.php
+++
@@ -1,2 +0,0 @@
-<script src="<?= base_url("assets/libs/jquery/jquery.min.js") ?>"></script>
-<script src="<?= base_url("assets/libs/bootstrap/js/bootstrap.bundle.min.js") ?>"></script>
